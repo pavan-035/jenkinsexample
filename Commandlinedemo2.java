4936eb8228fa24e9e14a5c441c43b80aa33f2ae4
pavan-035 <mandhap11@gmail.com>
2025-03-29 12:43:07 +0530

    Add files via upload

---
+++ Commandlinedemo2.java
@@ -0,0 +1,12 @@
+class Commandlinedemo1
+{
+public static void main(String args[])
+{
+String s1=args[0];
+String s2=args[0];
+String s3=args[0];
+System.out.println("name"+s1);
+System.out.println("roll no"+s2);
+System.out.println("department"+s3);
+}
+}
