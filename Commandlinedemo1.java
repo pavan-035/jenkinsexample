799277ef46f02a962de3a73e6b4287d5ab4f5a49
pavan-035 <mandhap11@gmail.com>
2025-03-29 12:12:54 +0530

    Add files via upload

---
+++ Commandlinedemo1.java
@@ -0,0 +1,16 @@
+class Commandlinedemo1
+{
+public static void main(String args[])
+{
+int n1=Integer.parseInt(args[0]);
+int n1=Integer.parseInt(args[1]);
+int r1=n1+n2;
+int r2=n1-n2;
+int r3=n1*n2;
+int r4=n1/n2;
+System.out.println("addition"+r1);
+System.out.println("substract"+r2);
+System.out.println("multiple"+r3);
+System.out.println("division"+r4);
+}
+}
